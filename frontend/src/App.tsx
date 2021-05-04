1e4e43fb9409b3d6859b903a7138c0d7a202b577
adrianof <adrianof@cpqd.com.br>
2021-05-04 14:13:44 -0300

    Incluindo bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>E aí mudão gordo sem fronteira!!!</h1>
+      <h1 className="text-primary">E aí mudão gordo sem fronteira!!!</h1>
     </div>
   );
 }
