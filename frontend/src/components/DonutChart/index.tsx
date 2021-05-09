286a1a92b1e518edc423d4b3bf5cc9d87773ebe0
adrianof <adrianof@cpqd.com.br>
2021-05-09 16:01:58 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -68,10 +68,10 @@ function DonutChart() {
     // de uma vez.
 
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    //const mockData = {
+     //   series: [477138, 499928, 444867, 220426, 473088],
+    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //}
 
     const options = {
         legend: {
