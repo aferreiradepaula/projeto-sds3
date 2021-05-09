2b46249cee32f933aa34b8afd2870519c18e09ba
adrianof <adrianof@cpqd.com.br>
2021-05-09 17:26:40 -0300

    Env var

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = "http://localhost:10550"
+// export const BASE_URL = "http://localhost:10550"
+
+
+// BACKEND_APP foi definido no netlify com a url do backend no heroku.
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:10550'
